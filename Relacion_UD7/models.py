cea251a0cba834ec105918bea4377bf92343e18b
Axas11 <frarodgue1234@gmail.com>
2025-03-26 22:29:34 +0100

    prueba clase

---
+++ Relacion_UD7/models.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+@dataclass
+class Comercial:
+    id: int
+    nombre: str
+    apellido1: str
+    apellido2: str
+    ciudad: str
+    comision: float = 0
+
+@dataclass
+class Pedido:
+    id: int
+    cantidad: float
+    fecha: datetime
+    id_cliente: int
+    id_comercial: int
+
+@dataclass
+class Cliente:
+    id: int
+    nombre: str
+    apellido1: str
+    apellido2: str
+    ciudad: str
+    categoria: int
+
